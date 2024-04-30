62ca29cca154c7402cd0acd0e5dcf5cf7114f2da
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-30 18:50:37 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George Khalil
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use-strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+
+function checkEligibility() {
+
+  const userAge = parseInt(document.getElementById("user-Age").value)
+  const dayOfWeek = parseInt(document.getElementById("day-Of-Week").value)
+}
